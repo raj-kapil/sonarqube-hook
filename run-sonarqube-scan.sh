e3f06543152feb701dea7fabdcb1a0026dd3a73d
raj-kapil <kapil.raj@vogo.in>
2024-10-15 17:50:17 +0530

    doesn't require sonar org params in case of self hosted sonarqube

--- run-sonarqube-scan.sh
+++ run-sonarqube-scan.sh
@@ -2,16 +2,21 @@
 
 # Use environment variables if available, otherwise fallback to defaults
 SONAR_PROJECT_KEY=${SONAR_PROJECT_KEY:-"default-project-key"}
-SONAR_ORGANIZATION=${SONAR_ORGANIZATION:-"default-organization"}
+# SONAR_ORGANIZATION=${SONAR_ORGANIZATION:-"default-organization"} // in case using self-hosted sonarqube
 SONAR_HOST_URL=${SONAR_HOST_URL:-"https://sonarcloud.io"}
 SONAR_LOGIN=${SONAR_LOGIN:-"<your-sonarcloud-token>"}
 
-# Run SonarQube scan
+echo "Connecting to sonarqube..."
+echo "Project: $SONAR_PROJECT_KEY \n" 
+# echo "Org: $SONAR_ORGANIZATION \n" 
+echo "Host: $SONAR_HOST_URL \n" 
+echo "Token: $SONAR_LOGIN \n" 
+
 echo "Running SonarQube scan before commit..."
 
 mvn clean verify sonar:sonar \
   -Dsonar.projectKey=$SONAR_PROJECT_KEY \
-  -Dsonar.organization=$SONAR_ORGANIZATION \
+  # -Dsonar.organization=$SONAR_ORGANIZATION \
   -Dsonar.host.url=$SONAR_HOST_URL \
   -Dsonar.login=$SONAR_LOGIN
 
